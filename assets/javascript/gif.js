f7359ca03a499dcf7e16ab808105ead41e8bbb66
Dane <56905214+dfmattern@users.noreply.github.com>
2020-01-17 12:59:45 -0700

    created get and display  image from API function

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,15 +1,47 @@
-$(document).ready(function(){
-    
-let topics = [];
+$(document).ready(function() {
+  //console.log("Ready!");
 
-function displayGif(){
+  let topics = ["Lannister", "Stark", "Targaryen", "Baratheon"];
+
+  function displayGif() {
     $("#image-display").empty();
     let input = $(this).attr("data-name");
     let limit = 10;
+    let queryURL =
+      "https://api.giphy.com/v1/gifs/search?q=" +
+      input +
+      "&limit=" +
+      limit +
+      "&api_key=IljACbwiZbYfWHJLdecOLVBu4UFjI9d1";
 
-}
+    //perform GET request to the API
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      // console.log(response);
+      for (let i = 0; i < limit; i++) {
+        let display = $("<div>");
+        display.addClass("image-container");
 
+        let gif = $("<img>");
+        gif.attr("src", response.data[i].images.original_still.url);
+        gif.attr("gif-still", response.data[i].images.original_still.url);
+        gif.attr("gif-animated", response.data[i].images.original.url);
+        gif.attr("gif-state", "still");
+        gif.attr("class", "gif");
+        display.append(gif);
 
+        let gifRating = response.data[i].rating;
+        //console.log(rating);
+        let ratingDisplay = $("<p>").text("Rating" + gifRating);
+        display.append(ratingDisplay);
 
-//dont lose these
-})
+        $("#image-display").append(display);
+      }
+    });
+  }
+  
+
+  //dont lose these
+});
