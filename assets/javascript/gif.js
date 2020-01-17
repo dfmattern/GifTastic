8ddf96341a224226f55e5ad02184942135d1c9c8
Dane <56905214+dfmattern@users.noreply.github.com>
2020-01-17 11:56:15 -0700

    created html

---
+++ assets/javascript/gif.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    
+let topics = [];
+
+function displayGif(){
+    $("#image-display").empty();
+    let input = $(this).attr("data-name");
+    let limit = 10;
+
+}
+
+
+
+//dont lose these
+})
