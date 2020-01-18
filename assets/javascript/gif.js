a2c66bd41d1a59b16a20b1f6feb237d7b25d7029
Dane <56905214+dfmattern@users.noreply.github.com>
2020-01-17 22:51:54 -0700

    added jumbotron styled text added margin around buttons

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -19,10 +19,11 @@ $(document).ready(function() {
       url: queryURL,
       method: "GET"
     }).then(function(response) {
-      // console.log(response);
+      //console.log(response);
       for (let i = 0; i < limit; i++) {
         let display = $("<div>");
-        display.addClass("image-container");
+        display.addClass("gif-container")
+        //console.log(display);
 
         let gif = $("<img>");
         gif.attr("src", response.data[i].images.original_still.url);
@@ -31,43 +32,67 @@ $(document).ready(function() {
         gif.attr("gif-state", "still");
         gif.attr("class", "gif");
         display.append(gif);
+        console.log(gif);
 
         let gifRating = response.data[i].rating;
+      
         //console.log(rating);
-        let ratingDisplay = $("<p>").text("Rating" + gifRating);
+        let ratingDisplay = $("<p>").text("Rating: " + gifRating);
+        ratingDisplay.attr("class", "badge badge-secondary")
         display.append(ratingDisplay);
 
         $("#image-display").append(display);
+        console.log(response);
       }
     });
   }
+  //displayGif();
 
   function animateGif() {
     let currentState = $(this).attr("gif-state");
     let gifAnimate = $(this).attr("gif-animate");
     let gifStill = $(this).attr("gif-still");
 
-    if (currentState == "still"){
-        $(this).attr("src",gifAnimate);
-        $(this).attr("gif-state", "animate");
-    }
-    else if (currentState == "animate"){
-        $(this).attr("src",gifStill);
-        $(this).attr("gif-state", "still");
+    if (currentState == "still") {
+      $(this).attr("src", gifAnimate);
+      $(this).attr("gif-state", "animate");
+    } else if (currentState == "animate") {
+      $(this).attr("src", gifStill);
+      $(this).attr("gif-state", "still");
     }
   }
 
-  function makeButton(){
-      for(let j = 0; j < topics.length; j++){
-       let newButton = $("<button>" + topics[j] + "</button>")
-       newButton.attr("class", "btn btn-info");
-          newButton.appendTo("#display-buttons");
-      }
+  function makeButton() {
+    $("#display-buttons").empty();
+
+    for (let j = 0; j < topics.length; j++) {
+      let newButton = $("<button>" + topics[j] + "</button>");
+      newButton.attr("class", "btn btn-dark");
+      newButton.attr("id", "input");
+      newButton.attr("data-name", topics[j]);
+
+      $("#display-buttons").append(newButton);
+    }
   }
+
+  $("#submitButton").on("click", function() {
+    let input = $("#user-input")
+      .val()
+      .trim();
+      
+    form.reset();
+    topics.push(input);
+
+    makeButton();
+
+    return false;
+  });
+
   makeButton();
 
 
-  
-  
+  $("#display-buttons").on("click", "#input", displayGif);
+  $(document).on("click", ".gif", animateGif);
+
   //dont lose these
 });
